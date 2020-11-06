866baa0f61a905648fabf3a77bc9b84e3b07a868
AdamFoskit <afoskit@islander.tamucc.edu>
2020-11-06 12:01:12 -0600

    Updates

---
+++ precache-manifest.f1baaab5556a2a43889303e2f4605dea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e190f9163730580c2c2d4bb9ef88939",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "301d42f99e1d896d2fa0",
+    "url": "/my-portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9ab86a7237063f4e7426",
+    "url": "/my-portfolio/static/css/main.c6c8b690.chunk.css"
+  },
+  {
+    "revision": "301d42f99e1d896d2fa0",
+    "url": "/my-portfolio/static/js/2.176ae0a7.chunk.js"
+  },
+  {
+    "revision": "fd10926fe0a127bd91345d2564369c3a",
+    "url": "/my-portfolio/static/js/2.176ae0a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab86a7237063f4e7426",
+    "url": "/my-portfolio/static/js/main.7f25fcaa.chunk.js"
+  },
+  {
+    "revision": "6d7a7337562760299d40",
+    "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
+  },
+  {
+    "revision": "cc68a54c4d368d234e7c5c4b433f5a75",
+    "url": "/my-portfolio/static/media/Pro_Square.cc68a54c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.baa003092f11d6a74c211fa78fe55579.js"
+  "/my-portfolio/precache-manifest.f1baaab5556a2a43889303e2f4605dea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
